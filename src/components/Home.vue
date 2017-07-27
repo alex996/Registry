993dc90423f841d58da8b1b33c06ec04441d3f26
Alex <alexnezhynsky@gmail.com>
2017-07-27 03:23:04 +0100

    Added npm deps

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-spaced">
+          Hero title
+        </h1>
+        <h2 class="subtitle">
+          Hero subtitle
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
